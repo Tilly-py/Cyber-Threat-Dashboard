c74c8552d17eece0bb3d1d22c35b7a4f9ca47e35
Tilly-py <alex.s.tilly@outlook.com>
2026-06-10 10:37:18 +0200

    Navbar
    Added a cool navbar

--- weathertop/src/App.tsx
+++ weathertop/src/App.tsx
@@ -1,8 +1,10 @@
 import DashboardPage from './components/DashboardPage/DasboardPage';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <DashboardPage />
     </div>
   );
